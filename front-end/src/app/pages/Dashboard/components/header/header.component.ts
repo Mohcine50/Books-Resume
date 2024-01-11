67604ca6029dd03a9923e6ff7a5aa50d9f178d39
shegami <mohcinela1998@gmail.com>
2024-01-11 23:23:35 +0100

    front-end: setup dashboard header

---
+++ front-end/src/app/pages/Dashboard/components/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faBookOpen,
+  faUser,
+  faSearch,
+} from '@fortawesome/free-solid-svg-icons';
+import { Observable, map, pipe } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../../services/auth.service';
+@Component({
+  selector: 'dashboard-header',
+  standalone: true,
+  imports: [FontAwesomeModule, RouterModule, CommonModule],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent implements OnInit {
+  faUser = faUser;
+  faBookOpen = faBookOpen;
+  faSearch = faSearch;
+  constructor(private authService: AuthService) {}
+  ngOnInit(): void {}
+
+  logout = () => {
+    this.authService.logout().subscribe(() => {
+      localStorage.removeItem('accessToken');
+    });
+  };
+}
--- front-end/src/app/pages/Dashboard/dashboard.component.ts
+++ front-end/src/app/pages/Dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
